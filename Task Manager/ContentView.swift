ff155895a3a50c85d8ba22cd02013cd0baf55d0d
RizaMamedow <riza.mamedow22@gmail.com>
2024-02-08 20:55:10 +0500

    Initial Commit

---
+++ Task Manager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Task Manager
+//
+//  Created by Riza Mamedov on 08.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
